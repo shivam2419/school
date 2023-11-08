29491a94d6640a36d32ef9558b3c0daeaeeed38c
Shivam Sharma <120017051+shivam2419@users.noreply.github.com>
2023-11-08 19:55:31 +0530

    Add files via upload

---
+++ school_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import home_form,contact,event,activity,notice,registration,inaugration,assembly,function,Pt
+
+# Register your models here.
+admin.site.register(home_form)
+admin.site.register(contact)
+admin.site.register(event)
+admin.site.register(activity)
+admin.site.register(notice)
+admin.site.register(registration)
+admin.site.register(inaugration)
+admin.site.register(assembly)
+admin.site.register(function)
+admin.site.register(Pt)
